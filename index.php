e31d94481abd62ab0a647fdc0dafe7a48ddc1373
Aarón <Aarn@Aarn>
2017-03-13 20:25:52 +0100

    “cookies3 commit 1”

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
-<html> 
-    <head> 
-        <meta charset="UTF­8"> 
-        <title>Desplegant una aplicació PHP sobre Heroku</title> 
-    </head> 
-    <body> 
-        <?php 
-            echo "Afegint Cookies<br>"; 
-            echo "Sóc el desenvolupador aasama1617@gmail.com"; 
-            setcookie("nom", "Jaume Pons", time()+3600, "/", "", 0); 
-            setcookie("edat", "20", time()+3600, "/", "", 0); 
-            setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
-            setcookie("curs", "2n", time()+3600, "/", "", 0); 
-            setcookie("any", "2016­2017", time()+3600, "/", "", 0);            
-        ?>  
-    </body>    
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Desplegant una aplicacióPHP sobre Heroku</title>
+  </head>
+  <body>
+      <?php 
+        echo "Afegint Cookies<br>";
+        echo "Sóc el desenvolupar aasama1617daw2@gmail.com";
+        setcookie("nom", "Jaume Pons", time()+3600, "/", "", 0);
+        setcookie("edat", "20", time()+3600, "/", "", 0);
+        setcookie("cicle", "DAW", time()+3600, "/", "", 0);
+        setcookie("curs", "2n", time()+3600, "/", "", 0);
+        setcookie("any", "2016-2017s", time()+3600, "/", "", 0);
+
+       ?>
+  </body>
 </html>
